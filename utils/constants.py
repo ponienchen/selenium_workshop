395fe599955c5bd2a86a29de254ae9804c20e67d
Peter Chen <ponienchen@gmail.com>
2019-01-27 21:21:49 -0800

    master
    Updated the name for the chromedriver log in
    /utils/constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -2,7 +2,7 @@ class WebDriverClientConstant:
 
     ENABLE_LOGS = True
     LOGS_FOLDER = '../logs'
-    CHROME_DRIVER_LOG = f'{LOGS_FOLDER}/chromedriver_log.txt'
+    CHROME_DRIVER_LOG = f'{LOGS_FOLDER}/chromedriver.log'
     SUPPORTED_BROWSERS = ['chrome']
     TIME_OUT = 5  # in seconds
     SERVICE_ARGS = {
